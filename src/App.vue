3f8e8ad515cfa679487f8db3c482a7c6d09af4ff
ka-shifuka <aryadewanggapuja@gmail.com>
2023-10-16 08:41:40 +0700

    configuration navbar

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,26 @@
 import Navbar from "./components/Navbar.vue";
 import Jumbotron from "./components/Jumbotron.vue";
 import { ref, onMounted } from "vue";
+
+onMounted(() => {
+  function isInViewPort(element) {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          console.log(entries, element);
+        } else {
+          console.log("no im ");
+        }
+      });
+    });
+    observer.observe(element);
+  }
+
+  setInterval(() => {
+    const h = document.querySelector("#home");
+    isInViewPort(h);
+  }, 1000);
+});
 </script>
 <!-- end script -->
 
@@ -19,7 +39,7 @@ import { ref, onMounted } from "vue";
     <!-- main header page -->
 
     <!-- fotter page -->
-    <section class="h-screen">footer</section>
+    <section class="h-[200vh]">footer</section>
     <!-- fotter page -->
   </div>
 </template>
